de22a398cc36329eea75320b898e88d39bb82482
hmmis <mislam6789@gmail.com>
2017-06-12 22:54:44 +0600

    AdminLTE-2.3.11 Dashboard Added

---
+++ application/controllers/Admin.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('dashboard');
+	}
+
+}
+
+/********************End of file********************/
+/**
+ * The File is created by
+ * @ H M Mohidul Islam (Shovon)
+ */
